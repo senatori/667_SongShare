d588c508197cbab2661a5dacbe5c375548f3a832
Michael <michaelsanter@me.com>
2013-11-15 17:45:16 -0800

    controllers for artists, albums, songs

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 GroupProject::Application.routes.draw do
+
+  resources :albums
+  resources :artists
+  resources :songs
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
