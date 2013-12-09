e43b69b882fc3d5c41a5016f2bd43a3ba0556ec3
Michael <michaelsanter@me.com>
2013-12-09 14:48:26 -0800

    Albums and playlist only show 4 per row now. Added 'create new album' link on artist page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,6 @@ class SessionsController < ApplicationController
 
 	#used for facebook auth
   def create
-  	puts '*********HERE*********'
     fan = Fan.from_omniauth(env["omniauth.auth"])
     session[:fan_id] = fan.id
     redirect_to root_url
