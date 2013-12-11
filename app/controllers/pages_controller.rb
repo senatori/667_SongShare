ebd571d065c28240e053cf22cb521f81c9bd80ba
BaconALaCarte <mbacon@hiddenchaos.com>
2013-12-10 22:52:12 -0800

    Okay, here we go on that index page!

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,8 +3,6 @@ class PagesController < ApplicationController
 	def index
 		@albums = Album.order(created_at: :desc)
 		@albums2 = Album.order(release_date: :desc)
-		@artist = Artist.find(@albums.artist_id)
-		@artist2 = Artist.find(@albums2.artist_id)
 	end
 	
 	def artist
